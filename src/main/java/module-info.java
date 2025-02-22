37ea423b06745b38a26bfb0ed54f5067574726ea
Werner Wallman <tux@good-omens.se>
2025-02-22 03:03:41 +0100

    Reworked the Moddrop so it works properly now, was looking at the wrong part of the droptables.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module relicCruncher {
+	exports se.good_omens.relicCruncher;
+
+	requires java.net.http;
+	requires java.xml;
+	requires javafx.base;
+	requires javafx.controls;
+	requires javafx.graphics;
+	requires org.jetbrains.annotations;
+}
